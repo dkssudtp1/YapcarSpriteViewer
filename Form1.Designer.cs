63bedb23a94f13278cd82e6d49243534ce80414c
dkssudtp1 <dkssudtp1@naver.com>
2024-07-16 20:31:35 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace YapcarSpriteViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(315, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "경로";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "선택";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("굴림", 12F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.listBox1.Location = new System.Drawing.Point(60, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 468);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(491, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 600);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 695);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YapcarSpriteViewer
+{
+    public partial class Form1 : Form
+    {
+        public List<Sprite> SpriteList = new List<Sprite>();
+        public int type = 0;
+        public int imageCount = 0;
+        public float zoomFactor = 1.0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseWheel += pictureBox1_MouseWheel;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                SpriteList.Clear();
+                listBox1.Items.Clear();
+                panel1.Refresh();
+                pictureBox1.Refresh();
+                openFileDialog.Filter = "spr files (*.spr)|*.spr|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    textBox1.Text = openFileDialog.FileName;
+                    using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                    {
+                        parsedSprite(reader);
+                    }
+                }
+            }
+        }
+
+
+        public void parsedSprite(BinaryReader reader)
+        {
+            //얍카 SPR 파일인지 체크
+            if (!System.Text.Encoding.ASCII.GetString(reader.ReadBytes(4)).Equals("ISPR"))
+            {
+                return;
+            }
+
+            
+            type = reader.ReadInt32(); //0 = RGB565, 1 = RGBA4444 
+
+            reader.ReadInt32(); //unknown
+            imageCount = reader.ReadInt16();
+            reader.ReadInt16(); //unknown
+            reader.ReadBytes(408); //unknown
+            for (int i = 0; i < imageCount; i++)
+            {
+                Sprite sprite = new Sprite();
+                sprite.Type = type;
+                sprite.Width = reader.ReadInt16();
+                sprite.Height = reader.ReadInt16();
+                SpriteList.Add(sprite);
+
+                listBox1.Items.Add(i+1);
+            }
+            reader.ReadBytes(14 * imageCount);
+
+            for (int i = 0; i < imageCount; i++)
+            {
+                SpriteList[i].Data = reader.ReadBytes(SpriteList[i].Width * SpriteList[i].Height * 2);
+            }
+
+            Bitmap bitmap = CreateImageFromSprite(SpriteList[0]);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedItem != null)
+                {
+                    if (listBox1.SelectedIndex < SpriteList.Count)
+                    {
+                        Bitmap bitmap = CreateImageFromSprite(SpriteList[listBox1.SelectedIndex]);
+                        pictureBox1.Width = bitmap.Width;
+                        pictureBox1.Height = bitmap.Height;
+                        pictureBox1.Image = bitmap;
+                    }
+                }
+            }
+            catch { }
+        }
+
+        public Bitmap CreateImageFromSprite(Sprite sprite)
+        {
+            if (sprite.Data == null || sprite.Data.Length != sprite.Width * sprite.Height * 2)
+            {
+                return null;
+            }
+
+            Bitmap image = new Bitmap(sprite.Width, sprite.Height, PixelFormat.Format32bppArgb);
+            int dataIndex = 0;
+
+            for (int y = 0; y < sprite.Height; y++)
+            {
+                for (int x = 0; x < sprite.Width; x++)
+                {
+                    ushort pixelData = (ushort)(sprite.Data[dataIndex++] | (sprite.Data[dataIndex++] << 8));
+                    Color color = Color.Empty;
+
+                    switch (sprite.Type)
+                    {
+                        case 1: // RGBA4444
+                            color = DecodeRGBA4444(pixelData);
+                            break;
+                        case 0: // RGB565
+                            color = DecodeRGB565(pixelData);
+                            break;
+                    }
+
+                    image.SetPixel(x, y, color);
+                }
+            }
+
+            if (zoomFactor != 1.0f)
+            {
+                return ResizeImage(image, zoomFactor);
+            }
+
+            return image;
+        }
+
+        private Color DecodeRGBA4444(ushort pixelData)
+        {
+            int a = ((pixelData >> 12) & 0xF) * 255 / 15;
+            int r = ((pixelData >> 8) & 0xF) * 255 / 15;
+            int g = ((pixelData >> 4) & 0xF) * 255 / 15;
+            int b = ((pixelData >> 0) & 0xF) * 255 / 15;
+
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        private Color DecodeRGB565(ushort pixelData)
+        {
+            //배경 흰색으로 변경
+            if (pixelData == 0xF81F)
+            {
+                pixelData = 0xFFFF;
+            }
+            int b = ((pixelData >> 0) & 0x1F) * 255 / 31;
+            int g = ((pixelData >> 5) & 0x3F) * 255 / 63;
+            int r = ((pixelData >> 11) & 0x1F) * 255 / 31;
+
+            return Color.FromArgb(0xFF, r, g, b);
+        }
+
+        private Bitmap ResizeImage(Bitmap image, float zoomFactor)
+        {
+            int newWidth = (int)Math.Round(image.Width * zoomFactor);
+            int newHeight = (int)Math.Round(image.Height * zoomFactor);
+
+            Bitmap resizedBitmap = new Bitmap(newWidth, newHeight);
+            using (Graphics g = Graphics.FromImage(resizedBitmap))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return resizedBitmap;
+        }
+
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // 휠 회전 방향 확인
+            if (e.Delta > 0)
+            {
+                zoomFactor += 0.1f; // 확대 비율 조절
+            }
+            else
+            {
+                zoomFactor -= 0.1f; // 축소 비율 조절
+            }
+
+            // 최소/최대 배율 제한
+            zoomFactor = Math.Max(0.1f, zoomFactor);
+            zoomFactor = Math.Min(10.0f, zoomFactor);
+
+            //리스트 선택 안했을시 강제로 0
+            if (listBox1.SelectedIndex == -1)
+            {
+                listBox1.SelectedIndex = 0;
+            }
+
+            Bitmap bitmap = CreateImageFromSprite(SpriteList[listBox1.SelectedIndex]);
+            pictureBox1.Image = bitmap;
+        }
+    }
+
+
+    public class Sprite
+    {
+        public int Type { get; set; }
+        public short Width { get; set;}
+        public short Height { get; set;}
+        public byte[] Data { get; set; }
+    }
+
+}
